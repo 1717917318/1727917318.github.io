d07e008798aab160551f093b6adce1cfe73a56b2
jianting <jianting112358@gmail.com>
2018-02-21 22:43:05 +0800

    Create check.js

---
+++ check.js
@@ -0,0 +1,11 @@
+alert("hi"); 
+var n_id=document.getElementById('input_id');
+var n_pas=document.getElementById('input_pas');
+var s1=n_id.value;
+var s2=n_pas.value;
+if(s1=="123"&& s2=="123") alert("login success!");
+else {
+    alert("login failed!");
+}
+
+
